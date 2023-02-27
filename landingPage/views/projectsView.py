579cefbab41c280ae64657bae213ecfc9aa28838
Vítor Carrara <vitorcarrara1998@gmail.com>
2023-02-27 10:36:24 -0300

    Templates and Static

--- landingPage/views/projectsView.py
+++ landingPage/views/projectsView.py
@@ -1,22 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from landingPage.models import Project
 
 def project_view(request, id=None): 
     #id = request.GET.get("id")
     projects = Project.objects.all()
     
-    
-    if id == None: 
-        response = ""
-        for id in range(1, 6):
-            project = projects.get(id=id)
-            response += " / " + str(project) + " \ " 
 
-        print(projects)
-        return HttpResponse(f'<h1> Meus Projetos <h1> {response}' )
-    else:
-        project = projects.get(id=id) 
-        return HttpResponse(f'<h1> Projeto #{id} <h1>')
+    if id == None: 
+        return render(request, template_name='projects/projects.html', status=200)
+    else: 
+        return render(request, template_name='projects/project.html', status=200)
 
 
     
